3e6fc9a0c3644d9ac7ec28cecf9b6162066c1833
Nicholas Fagner <nicholasfagner@gmail.com>
2021-12-17 12:12:20 -0700

    commit tree cfcec76b8757f5a81af6a88ba91c5bac626f7dd0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    vaildates :title_must_have_clickbait
+
+    def title_must_have_clickbait
+        unless title.includes(["Won't Believe", "Secret", "Top #{[number]}", "Guess"])
+            errors.add(:title, "Must include clickbait!")
+        end
+    end
 end
